2a6b997c9ac7c1e3126bfb41632eb84ca31f5cda
Duy.T.C <duytran.81811@gmail.com>
2023-03-05 10:44:57 +0700

    Remove: @Bean Registry, ISpringBus

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -1,27 +1,12 @@
 package com.example.springsecurity;
 
-import io.cqrs.dispascher.ISpringBus;
-import io.cqrs.dispascher.Registry;
-import io.cqrs.dispascher.SpringBusImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
-    }
-
-    @Bean
-    public Registry registry(ApplicationContext applicationContext) {
-        return new Registry(applicationContext);
-    }
-
-    @Bean
-    public ISpringBus commandBus(Registry registry) {
-        return new SpringBusImpl(registry);
     }
 }
---
+++ src/main/java/com/example/springsecurity/adapters/Adapter.java
@@ -0,0 +1,7 @@
+package com.example.springsecurity.adapters;
+
+import org.lognet.springboot.grpc.GRpcService;
+
+@GRpcService
+public class Adapter {
+}

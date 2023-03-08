d81642ef3b7fbaba3a2e4052ca1f785474007d26
Duy.T.C <duytran.81811@gmail.com>
2023-03-08 22:01:00 +0700

    add: grpc

--- src/main/java/com/example/springsecurity/adapters/Adapter.java
+++ src/main/java/com/example/springsecurity/adapters/Adapter.java
@@ -1,7 +1,9 @@
 package com.example.springsecurity.adapters;
 
+import com.grpc.examples.BookAuthorServiceGrpc;
 import org.lognet.springboot.grpc.GRpcService;
 
 @GRpcService
-public class Adapter {
+public class Adapter extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
+
 }

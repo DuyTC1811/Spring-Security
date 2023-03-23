e87b1a2aa1b3cf2b7749088c78f3827724c1a2e8
Duy.T.C <duytran.81811@gmail.com>
2023-03-23 10:57:59 +0700

    change: login -> sign-in

--- src/main/java/com/example/springsecurity/controllers/queries/LoginQueryCtrl.java
+++ src/main/java/com/example/springsecurity/controllers/queries/LoginQueryCtrl.java
@@ -5,10 +5,7 @@ import com.example.springsecurity.dto.response.LoginResponse;
 import io.cqrs.controller.QueryController;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginQueryCtrl extends QueryController<LoginResponse, LoginRequest> {
 
     @Override
-    @GetMapping("/login")
+    @PostMapping("/sign-in")
     protected ResponseEntity<LoginResponse> coordinator(@RequestBody LoginRequest loginRequest) {
         return execute(loginRequest);
     }

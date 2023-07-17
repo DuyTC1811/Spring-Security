62b4d9730cd4401dc8053a7ddc0d4fbc8067b17f
DuyTC1811 <duytran.81811@gmail.com>
2023-07-17 20:06:08 +0700

    add: spring cloud

--- src/main/java/com/example/springsecurity/adapters/Adapter.java
+++ src/main/java/com/example/springsecurity/adapters/Adapter.java
@@ -1,9 +1,9 @@
-package com.example.springsecurity.adapters;
-
-import com.grpc.examples.BookAuthorServiceGrpc;
-import org.lognet.springboot.grpc.GRpcService;
-
-@GRpcService
-public class Adapter extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
-
-}
+//package com.example.springsecurity.adapters;
+//
+//import com.grpc.examples.BookAuthorServiceGrpc;
+//import org.lognet.springboot.grpc.GRpcService;
+//
+//@GRpcService
+//public class Adapter extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
+//
+//}
